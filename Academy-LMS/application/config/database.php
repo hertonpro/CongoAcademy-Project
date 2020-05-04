6ce4ea9672c4053d2e46dc0cb6d74519d0fa87aa
hertonpro <alvantyomihigo@gmail.com>
2020-05-04 09:28:43 +0200

    Changement du logo
    Changement du logo

--- Academy-LMS/application/config/database.php
+++ Academy-LMS/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'db_host',
-	'username' => 'db_user',
-	'password' => 'db_pass',
-	'database' => 'db_name',
+	'hostname' => '127.0.0.1',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'congoacademy',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
